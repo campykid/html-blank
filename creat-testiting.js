23c0f099b344dc143ad3b2f8818852eaff73423b
vladislav khvostov <vladkhvo@gmail.com>
2015-04-30 09:34:35 +0300

    v0.0.2Добавляет поле для стилей и скриптов

--- creat-testiting.js
+++ creat-testiting.js
@@ -1,19 +1,25 @@
 
-// Заполнить болванкой
+// Болванка.
 var skelet = [ 
 '<!DOCTYPE html>',
 '\n<html>',
 '\n<head>\n\t<meta charset="UTF-8">',
 '\n\t<title>Document</title>',
+'\n\t<style>',
+'\n',
+'\n\t</style>',
 '\n</head>',
 '\n\t<body>',
 '\n',	
 '\n\t</body>',
+'\n\t<script type="text/javascript">',
+'\n',
+'\n\t</script>',
 '\n</html>'
 ].join('')
 
 
-// Созадать файл
+// Созадает файл с болванкой.
 var fs = require('fs');
 fs.writeFile("index.html ", skelet, function(err) {
     if(err) throw err;
