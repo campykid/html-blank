0a55b02f229741b1b6f6e600d0155c7334a1f8c2
vladislav <vladkhvo@gmail.com>
2015-05-16 09:35:45 +0300

    Добавлена генерация ссылки на Jquery

--- creat-testiting.js
+++ creat-testiting.js
@@ -1,11 +1,22 @@
 
 var fs = require('fs');
 
+// Добавляет Jquery, если есть аргумент j или Jquery.
+var Jquery = null;
+for (var i = process.argv.length - 1; i >= 0; i--) {
+	if (process.argv[i] === 'j' || process.argv[i] === 'Jquery' || process.argv[i] === '-j' ) {
+		Jquery  = '\n\t<script src="http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script>'
+	};
+};
+
 // Болванка.
 var skelet = [ 
 '<!DOCTYPE html>',
 '\n<html>',
 '\n<head>\n\t<meta charset="UTF-8">',
+
+Jquery,
+
 '\n\t<title>Document</title>',
 '\n\t<style>',
 '\n',
@@ -21,7 +32,7 @@ var skelet = [
 ].join('')
 
 // Путь к файлу - если задан аргументом.
-var path = process.argv[2] ? path = process.argv[2]: path = '' ;
+var path = process.argv[2].charAt(0) === '/' ? path = process.argv[2]: path = '' ;
 
 // Созадает файл с болванкой.
 // Если переменная path пуста, создает файл в директории с программой.
@@ -29,3 +40,4 @@ fs.writeFile(path + "index.html", skelet, function(err) {
     if(err) throw err;
     console.log("The file was created!");
 });
+
